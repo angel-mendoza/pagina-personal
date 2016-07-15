1e63ed096a62c6ce27cbc5c149e2bb01feb0fa04
angel_mendoza_daboin <mendozaangel003@gmail.com>
2016-07-15 23:45:38 +0000

    se arregle el controlador de articulo y ya esta funcional el contador de visitas

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :autehenticate_user!, except: [:show, :index] 
+    before_action :set_article, except: [:index, :new, :create]
     def index
         @articles = Article.all 
     end
     
     def show
-        @article = Article.find(params[:id])
+       @article.update_visits_count
     end
     
     def new
@@ -12,11 +14,11 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+       
     end
     
     def create
-        @article = Article.new(article_params)
+        @article = current_user.articles.new(article_params)
         if @article.save
             redirect_to @article
         else
@@ -25,13 +27,12 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
     
     def update
-        @article = Article.find(params[:id])
+
         if @article.update(article_params)
             redirect_to @article
         else
@@ -40,6 +41,10 @@ class ArticlesController < ApplicationController
     end
     
     private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
     
     def article_params
         params.require(:article).permit(:title, :body)
